18b714d194cc9c8b5393fe3a5a20ec0db3b85a0e
Simon Strassl <simon.strassl@gmail.com>
2013-05-30 14:27:10 +0200

    Refactor into Monad

--- bot.hs
+++ bot.hs
@@ -1,4 +1,7 @@
+import Control.Arrow
 import Control.Monad
+import Control.Monad.Reader
+import Control.Exception
 import Data.Char
 import Data.List
 import Network
@@ -12,43 +15,61 @@ data Command = Command { action :: String
                        , args   :: [String]
                        } deriving Show
 
+data Bot = Bot { socket :: Handle }
+
+type Net = ReaderT Bot IO
+
 server = "irc.freenode.org"
 port = 6667
 chan = "#hbot-test"
 nick = "hbotty"
 
-main = do
+
+main = bracket connect disconnect loop
+    where
+        loop st = runReaderT run st
+
+connect :: IO Bot
+connect = do 
     h <- connectTo server (PortNumber (fromIntegral port))
     hSetBuffering h NoBuffering
-    write h (cmdNick nick)
-    write h (cmdUser nick)
-    write h (cmdJoin chan)
-    listen h
+    return (Bot h)
 
-write :: Handle -> Message -> IO ()
-write h m = do
-        hPrintf h str
-        printf    str
+disconnect = hClose . socket
+
+run :: Net ()
+run = do
+    write (cmdNick nick)
+    write (cmdUser nick)
+    write (cmdJoin chan)
+    asks socket >>= listen
+
+
+write :: Message -> Net ()
+write m = do
+    h <- asks socket
+    liftIO $ hPrintf h str
+    liftIO $ printf    str
     where
         str = compose m ++ "\r\n"
 
-listen :: Handle -> IO ()
+listen :: Handle -> Net ()
 listen h = forever $ do
-        l <- hGetLine h
-        putStrLn l
-        processLine h l
+        l <- liftIO $ hGetLine h
+        liftIO $ putStrLn l
+        processLine l
 
-processLine :: Handle -> String -> IO ()
-processLine h s
-    | isPing msg = write h cmdPong
-    | otherwise = eval h msg
+processLine :: String -> Net ()
+processLine s
+    | isPing msg = write cmdPong
+    | otherwise = eval msg
     where 
         server = head $ words $ trailing msg
         msg = parse s
 
-eval :: Handle -> Message -> IO ()
-eval h m
-    | isBotCommand m = processBotCommand h m
+eval :: Message -> Net ()
+eval m
+    | isBotCommand m = processBotCommand m
     | otherwise = return ()
 
 isBotCommand :: Message -> Bool
@@ -56,11 +77,11 @@ isBotCommand m = ("!" ++ nick) `isPrefixOf` s
     where
         s = trailing m
 
-processBotCommand :: Handle -> Message -> IO ()
-processBotCommand h m
-    | act == "SAY" = write h (cmdMsg chan (intercalate " " (args c)))
-    | act == "QUIT" = write h (cmdQuit "Going down!")
-    | otherwise = write h (Message "" "PRIVMSG" [chan] ("I'm sorry, " ++ prefix m ++ ". I'm afraid I can't do that."))
+processBotCommand :: Message -> Net ()
+processBotCommand m
+    | act == "SAY" = write (cmdMsg chan (intercalate " " (args c)))
+    | act == "QUIT" = write (cmdQuit "Going down!")
+    | otherwise = write (Message "" "PRIVMSG" [chan] ("I'm sorry, " ++ prefix m ++ ". I'm afraid I can't do that."))
     where
         act = action c
         c = parseCommand m
