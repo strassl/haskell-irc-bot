6e9228365a6d86d5964b34952bd3f6452f0f96b1
Simon Strassl <simon.strassl@gmail.com>
2013-05-30 20:38:39 +0200

    Implement a whitelist for user who are allowed to issue commands

--- bot.hs
+++ bot.hs
@@ -1,4 +1,5 @@
 import Control.Arrow
+import Control.Applicative
 import Control.Monad
 import Control.Monad.Reader
 import Control.Exception
@@ -16,14 +17,17 @@ data Command = Command { action :: String
                        } deriving Show
 
 data Bot = Bot { socket :: Handle 
-               , active :: Bool }
+               , operators :: [String]
+               }
 
 type Net = ReaderT Bot IO
 
 server = "irc.freenode.org"
 port = 6667
 chan = "#hbot-test"
-nick = "hbotty"
+nickname = "hbotty"
+
+operatorsFile = "operators.txt"
 
 -- bracket <before> <after> <in between>
 main = bracket connect disconnect loop
@@ -34,25 +38,26 @@ main = bracket connect disconnect loop
 
 connect :: IO Bot
 connect = do 
+    ops <- getOperators
     h <- connectTo server (PortNumber (fromIntegral port))
     hSetBuffering h NoBuffering
-    return (Bot h)
+    return (Bot h ops)
 
 disconnect :: Bot -> IO ()
 disconnect = hClose . socket
 
 run :: Net ()
 run = do
-    write (cmdNick nick)
-    write (cmdUser nick)
+    write (cmdNick nickname)
+    write (cmdUser nickname)
     write (cmdJoin chan)
     asks socket >>= listen
 
 listen :: Handle -> Net ()
 listen h = forever $ do
-        l <- liftIO $ hGetLine h
-        liftIO $ putStrLn l
-        processLine l
+    l <- liftIO $ hGetLine h
+    liftIO $ putStrLn l
+    processLine l
 
 write :: Message -> Net ()
 write m = do
@@ -62,25 +67,46 @@ write m = do
     where
         str = compose m ++ "\r\n"
 
+-- Operator processing
+
+getOperators :: IO [String]
+getOperators = do
+    f <- readFile operatorsFile
+    return $ parseOperators f
+
+parseOperators :: String -> [String]
+parseOperators s = lines s
+
+isOperator :: Message -> Net Bool
+isOperator m = do
+    ops <- asks operators
+    return $ userInList m ops
+
+userInList :: Message -> [String] -> Bool
+userInList m l = elem (maybe "" nick u) l
+    where
+        u = parseUser (prefix m)
+
 -- Text processing
 
 processLine :: String -> Net ()
 processLine s
-    | isPing msg = write cmdPong
+    | isPing msg = write $ cmdPong (trailing msg)
     | otherwise = eval msg
     where 
         server = head $ words $ trailing msg
         msg = parse s
 
 eval :: Message -> Net ()
-eval m
-    | isBotCommand m = processBotCommand m
-    | otherwise = return ()
+eval m = do
+    isOp <- isOperator m
+
+    if isOp && isBotCommand m then processBotCommand m else return ()
 
 -- Bot command processing
 
 isBotCommand :: Message -> Bool
-isBotCommand m = ("!" ++ nick) `isPrefixOf` s
+isBotCommand m = ("!" ++ nickname) `isPrefixOf` s
     where
         s = trailing m
 
@@ -88,8 +114,9 @@ processBotCommand :: Message -> Net ()
 processBotCommand m
     | act == "SAY" = write (cmdMsg chan (intercalate " " (args c)))
     | act == "QUIT" = write (cmdQuit "Going down!")
-    | otherwise = write (Message "" "PRIVMSG" [chan] ("I'm sorry, " ++ prefix m ++ ". I'm afraid I can't do that."))
+    | otherwise = write (Message "" "PRIVMSG" [chan] ("I'm sorry, " ++ name ++ ". I'm afraid I can't do that."))
     where
+        name = maybe "" nick (parseUser (prefix m))
         act = action c
         c = parseCommand m
 
@@ -107,7 +134,7 @@ parseCommand m = parseArgs arguments
 
 cmdNick n = Message "" "NICK" [n] ""
 
-cmdUser n = Message "" "USER" [nick, "0", "*"] "bot"
+cmdUser n = Message "" "USER" [n, "0", "*"] "bot"
 
 cmdJoin c = Message "" "JOIN" [c] ""
 
@@ -115,7 +142,7 @@ cmdQuit m = Message "" "QUIT" [m] ""
 
 cmdMsg r m = Message "" "PRIVMSG" [r] m
 
-cmdPong = Message "" "PONG" [server] ""
+cmdPong s = Message "" "PONG" [s] ""
 
 isPing :: Message -> Bool
 isPing m = (command m) == "PING"
