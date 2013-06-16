9f4815c8e0319ef1ee3cee94ada12df88cee0cfd
Simon Strassl <simon.strassl@gmail.com>
2013-06-16 20:15:59 +0200

    Move global variables into the Net monad

--- bot.hs
+++ bot.hs
@@ -23,18 +23,20 @@ data Command = Command { action :: String
                        , args   :: [String]
                        } deriving Show
 
+data Config = Config { server   :: String
+                     , port     :: Int
+                     , chan     :: String
+                     , nickname :: String
+                     , password :: String
+                     } deriving Show
+
 data Bot = Bot { socket :: Handle 
+               , config :: Config
                , operators :: [String]
                , connection :: PQ.Connection
                }
 
 type Net = ReaderT Bot IO
-
-server = "irc.freenode.org"
-port = 6667
-chan = "#reddit"
-nickname = "hbotty"
-password = "password"
 
 operatorsFile = "operators.txt"
 
@@ -47,21 +49,23 @@ main = bracket connect disconnect loop
 
 connect :: IO Bot
 connect = do 
+    let conf = Config "irc.freenode.org" 6667 "#reddit" "hbotty" "password"
     ops <- getOperators
-    h <- connectTo server (PortNumber (fromIntegral port))
+    h <- connectTo (server conf) (PortNumber (fromIntegral (port conf)))
     c <- connectDB
     hSetBuffering h NoBuffering
-    return (Bot h ops c)
+    return (Bot h conf ops c)
 
 disconnect :: Bot -> IO ()
 disconnect = hClose . socket
 
 run :: Net ()
 run = do
-    write (cmdPass password)
-    write (cmdNick nickname)
-    write (cmdUser nickname)
-    write (cmdJoin chan)
+    conf <- asks config
+    write (cmdPass (password conf))
+    write (cmdNick (nickname conf))
+    write (cmdUser (nickname conf))
+    write (cmdJoin (chan conf))
     asks socket >>= listen
 
 listen :: Handle -> Net ()
@@ -84,20 +88,23 @@ logFilter :: [String]
 logFilter = [ "Excess Flood", "Client Quit", "Remote host closed", "*.net *.split", "Read error"
             , "Ping timeout", "Quit:" ]
 
-shouldLog :: Message -> Bool
-shouldLog m = ((command m) == "PRIVMSG") && 
-              (isUserPrefix $ prefix m) &&
-              (not $ isBotCommand m) &&
-              (not $ isEmpty (trailing m)) &&
-              (not $ or (map lFilter logFilter))
+shouldLog :: Config -> Message -> Bool
+shouldLog conf m = ((command m) == "PRIVMSG") && 
+                   (isUserPrefix $ prefix m) &&
+                   (not $ isBotCommand conf m) &&
+                   (not $ isEmpty (trailing m)) &&
+                   (not $ or (map lFilter logFilter))
     where
         lFilter = flip isInfixOf (trailing m)
         isEmpty s = (null s) || (and $ map isSpace s)
 
-logLine :: Message -> IO ()
-logLine m
-    | shouldLog m = appendFile (chan ++ "_log.txt") ((trailing m) ++ "\n")
-    | otherwise = return ()
+logLine :: Message -> Net ()
+logLine m = do
+    conf <- asks config
+
+    if shouldLog conf m
+        then liftIO $ appendFile ((chan conf) ++ "_log.txt") ((trailing m) ++ "\n")
+        else liftIO $ return ()
 
 -- Database stuff
 
@@ -139,23 +146,24 @@ processLine s
 eval :: Message -> Net ()
 eval m = do
     isOp <- isOperator m
-    liftIO $ logLine m
+    conf <- asks config
+    logLine m
 
-    if isOp && isBotCommand m then processBotCommand m else return ()
+    if isOp && isBotCommand conf m then processBotCommand conf m else return ()
 
 -- Bot command processing
 
-isBotCommand :: Message -> Bool
-isBotCommand m = ("!" ++ nickname) `isPrefixOf` s
+isBotCommand :: Config -> Message -> Bool
+isBotCommand conf m = ("!" ++ (nickname conf)) `isPrefixOf` s
     where
         s = trailing m
 
-processBotCommand :: Message -> Net ()
-processBotCommand m
-    | act == "SAY" = write (cmdMsg chan (intercalate " " (args c)))
+processBotCommand :: Config -> Message -> Net ()
+processBotCommand conf m
+    | act == "SAY" = write (cmdMsg (chan conf) (intercalate " " (args c)))
     | act == "QUIT" = write (cmdQuit "Going down!")
     | act == "CHAIN" = processMarkov c
-    | otherwise = write (Message "" "PRIVMSG" [chan] ("I'm sorry, " ++ name ++ ". I'm afraid I can't do that."))
+    | otherwise = write (Message "" "PRIVMSG" [(chan conf)] ("I'm sorry, " ++ name ++ ". I'm afraid I can't do that."))
     where
         name = maybe "" nick (parseUser (prefix m))
         act = action c
@@ -174,9 +182,10 @@ parseCommand m = parseArgs arguments
 processMarkov :: Command -> Net ()
 processMarkov c = do
     db <- asks connection 
+    conf <- asks config
     txt <- liftIO $ createText db start num
 
-    write $ cmdMsg chan (T.unpack (T.replace "\n" "" txt))
+    write $ cmdMsg (chan conf) (T.unpack (T.replace "\n" "" txt))
 
     where
         a = args c
